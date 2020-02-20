3bda14cf2d0ae0b85cb7319023f5fd455c28648e
Christian Tenllado <tenllado@ucm.es>
2020-02-20 15:58:31 +0100

    removed unnecesary imports from features.py

--- src/solarnc/features.py
+++ src/solarnc/features.py
@@ -1,10 +1,4 @@
-import pysolar as psol
-#import pvlib
-from pvlib import clearsky
 from pvlib.location import Location
-
-from . import mcclear as mc
-import math
 
 # pvlib provides: ‘ineichen’, ‘haurwitz’, ‘simplified_solis'
 def csm_pvlib(df, stations, model, itype):
