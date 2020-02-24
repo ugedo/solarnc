02eeb422048817b79c6db6d1ab36a64ba61cc0ef
Christian Tenllado <tenllado@ucm.es>
2020-02-24 15:46:53 +0100

    clear sky index, elevation considered

--- src/solarnc.py
+++ src/solarnc.py
@@ -3,8 +3,10 @@ import os
 import csv
 import pandas as pd
 import jsonschema as jsch
+#import pvlib
 from pvlib.location import Location
 import multiprocessing as mp
+#import numpy as np
 
 def load_config(fname):
     with open(fname, 'r') as f:
@@ -37,13 +39,22 @@ def read_list(fname):
     return l[0]
 
 # pvlib provides: ‘ineichen’, ‘haurwitz’, ‘simplified_solis'
-def csm_pvlib(df, skip_existing, name, stations, model, itype):
+def csm_pvlib(df, skip_existing, name, stations,\
+        model, itype, kprefix, decimals):
     for sta in stations:
-        colname = "{} {}".format(name, sta["name"])
-        if skip_existing and colname in df.columns:
+        staname = sta['name']
+        ghicol = "GHI {}".format(staname)
+        csmcol = "{} {}".format(name, staname)
+        kcol = "{}{} {}".format(kprefix, name, staname)
+        if skip_existing and csmcol in df.columns:
             continue
-        loc = Location(sta["latitude"], sta["longitude"])
-        df[colname] = loc.get_clearsky(df.index, model = model)[itype]
+        latitude = sta["latitude"]
+        longitude = sta["longitude"]
+        altitude = sta["MASL"] if "MASL" in sta else 0
+        loc = Location(latitude, longitude, df.index.tz.zone, altitude)
+        df[csmcol] = round(loc.get_clearsky(df.index, model = model)[itype]\
+                    , decimals)
+        df[kcol] = round(df[ghicol]/df[csmcol], decimals)
 
 def run_cbk_unpack(tup):
     cbk, argtup = tup
