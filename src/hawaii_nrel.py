e6c9f8887f9993767619b1cd579b26de578ad8b9
Christian Tenllado <tenllado@ucm.es>
2020-03-03 12:27:00 +0100

    format hawaii using pandas, filter negatives

--- src/hawaii_nrel.py
+++ src/hawaii_nrel.py
@@ -5,6 +5,7 @@ import os
 import multiprocessing as mp
 import solarnc as snc
 import glob
+import pandas as pd
 
 # original columns in the file
 data_columns = ["Seconds", "Year", "DOY", "HST",
@@ -28,34 +29,26 @@ data_columns = ["Seconds", "Year", "DOY", "HST",
         "GHI DH7",
         "GHI DH8"]
 
-def format_data(infile,outpath, ghi_index, header, start, end, rejectpath):
+def format_data(infile,outpath, ghi_columns, rejectpath):
     base = os.path.basename(infile)
     day = os.path.splitext(base)[0]
-    numlines = 0
-    nneg = [0] * 16
-    ifile = open(infile, 'r')
-    ofilename = "{}/{}.csv".format(outpath, day)
-    ofile = open(ofilename, 'w')
-    ofile.write(header)
+    df = pd.read_csv(infile, header=None, names=data_columns)
 
-    reader = csv.reader(ifile)
-    for f in reader:
-        numlines += 1
-        if int(f[3]) < start or int(f[3]) > end:
-            continue
-        hh = int(f[3][:-2])
-        mm = int(f[3][-2:])
-        dtime = dt.datetime(int(f[1]), 1, 1,hh,mm,int(f[0])) + dt.timedelta(int(f[2]) - 1)
-        outline = "{}-10:00,".format(dtime)
-        ghis = [float(f[g]) for g in ghi_index]
-        nneg = [nneg[i] + 1 if ghis[i] < 0.0 else nneg[i] for i in range(16)]
-        outline += ','.join(["{}".format(g) for g in ghis])
-        ofile.write(outline + '\n')
-
-    ifile.close()
-    ofile.close()
-    if any(x >= 0.1*numlines for x in nneg):
-        os.rename(ofilename, "{}/{}.csv".format(rejectpath, day))
+    dt = pd.to_datetime(df.Seconds.apply(str) + ' ' + df.Year.apply(str) + ' '\
+            + df.DOY.apply(str) + ' ' + df.HST.apply(str), \
+            format='%S %Y %j %H%M')
+    dt = dt.dt.tz_localize('HST')
+    df = pd.DataFrame(dict([('datetime', dt)] + \
+            [(g, df[g]) for g in ghi_columns]))
+    df.set_index('datetime', inplace=True)
+    # small negative values are truncated to 0
+    df[df.lt(0) & df.gt(-1)] = 0
+    # large negative values are errors
+    rem_neg_cols = df.lt(0).sum().gt(0).sum()
+    if rem_neg_cols > 0:
+        snc.save_csv(df, "{}/{}.csv".format(rejectpath, day))
+    else:
+        snc.save_csv(df, "{}/{}.csv".format(outpath, day))
 
 def nrelformat(dtset, fconfig, npjobs):
     path = dtset['path']
@@ -76,20 +69,6 @@ def nrelformat(dtset, fconfig, npjobs):
 
     stations = fconfig['stations']
     ghi_columns = ["GHI {}".format(sta) for sta in stations]
-    ghi_index = [data_columns.index(c) for c in ghi_columns]
 
-    if 'start' in fconfig:
-        start = fconfig['start']
-    else:
-        start = 730
-
-    if 'end' in fconfig:
-        end = fconfig['end']
-    else:
-        end = 1730
-
-    header = ','.join(["datetime"] + ghi_columns)
-    header = header + '\n'
-    arglist = [(f, outpath, ghi_index, header, start, end, rejectpath) \
-            for f in infiles]
+    arglist = [(f, outpath, ghi_columns, rejectpath) for f in infiles]
     snc.runjobs(format_data, arglist, npjobs)
