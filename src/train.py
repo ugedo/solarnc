56f2b697b679621ccb94c5f316a099e3505708d1
Christian Tenllado <tenllado@ucm.es>
2020-03-05 10:07:14 +0100

    wip working on the train stage

---
+++ src/train.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import optparse
+import os
+import solarnc as snc
+import glob
+import multiprocessing as mp
+import random
+
+def parse_options():
+    usage_str = "%prog -c json_config_file"
+    parser = optparse.OptionParser(usage_str)
+    parser.add_option("-c", dest="config", type="string",
+            help="path to the json file with the configuration parameters")
+
+    options, args  = parser.parse_args()
+    if not options.config:
+        parser.error("missing json config file")
+    return (options, args)
+
+def print_model(m):
+    print("* {} with outputs for {} and horizons {}".format(m['model type'], \
+            m['target stations'], m['horizons']))
+
+def main(options, args):
+    config = snc.load_config(options.config)
+    print("Correct config format")
+
+    fsconfig = config['fselect']
+    ffiles  = glob.glob("{}/*_features.csv".format(fsconfig['outpath']))
+    print("Input features files:")
+    print(ffiles)
+
+    tfiles  = glob.glob("{}/*_targets.csv".format(fsconfig['outpath']))
+    print("Input targets files:")
+    print(tfiles)
+
+    spconfig = config['split']
+    trainset_fname = "{}/trainset.csv".format(spconfig['outpath'])
+    trainset = snc.read_list(trainset_fname)
+    print("Trainset from {} is:".format(trainset_fname))
+    print(trainset)
+
+    tconfig = config['train']
+    models = tconfig['models']
+    for m in models:
+        print_model(m)
+
+
+
+
+if __name__=="__main__":
+    main(*parse_options())
