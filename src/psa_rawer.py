313ad406619d8c1c3bc1fbd54bb8301500d40658
Enrique Ugedo Egido <Enrique.Ugedo@ciemat.es>
2021-03-03 16:10:12 +0100

    incluído en psa_rawer.py guion de ideas de desarrollo para el paso de datos disponibles a un raw filtrado que posteriormente pasará por psa_nrel.py y al resto de la herramienta.

---
+++ src/psa_rawer.py
@@ -0,0 +1,6 @@
+# TODO obtener fecha comienzo de la muestra de datos, crear variable a las 0:00 de ese día
+# TODO obtener los datos de esa marca temporal para cada una de las estaciones, incrementar en minutos
+# TODO filtrar datos con valores físicamente imposible, según las indicaciones de la BSRN (mirar en el libro)
+# TODO generar log con los datos que se han filtrado y un código de error que indique el motivo
+# TODO incluir en el log los momentos para los que alguna estación no tiene datos registrados
+# TODO obtener datos de elevación solar y eliminar los registros con una elevación inferior a 7º
