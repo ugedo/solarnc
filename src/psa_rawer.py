a4f4a4b1f35b818241bb825c2d28e9388a952dad
Enrique Ugedo Egido <Enrique.Ugedo@ciemat.es>
2021-03-09 16:52:47 +0100

    Obtiene los datos de geometría solar de METAS con el objetivo de utilizarlos para filtrar en el resto de estaciones. Se guardan en el log los timestamps para los que no hay datos de METAS

--- src/psa_rawer.py
+++ src/psa_rawer.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 import solarnc as snc
+import pandas as pd
 import numpy as np
 import optparse
 import glob
@@ -19,6 +20,21 @@ def parse_options():
     if not options.config:
         parser.error("missing json config file")
     return options, args
+
+
+def getFromFile(f):
+    # Apertura y carga del fichero en DataFrame
+    df = pd.read_csv(f, header=[1, 2, 3])
+    # Elimina columnas que no vamos a usar
+    # df.drop(columns=["RN"], level=1, inplace=True)
+    # Remplaza los 0 por NaN para omitirlos en el cálculo
+    # df.replace(0, np.nan, inplace=True)
+    df[df == 0] = np.nan
+    # Remplaza las cadenas "NAN" por el valor np.nan (NaN de numpy)
+    # df.replace("NAN", np.nan, inplace=True)
+    # Covienrte la columna "TIMESTAMP" a tipo datatime
+    df[["TIMESTAMP"]] = df[["TIMESTAMP"]].astype(np.datetime64)
+    return df
 
 
 # TODO filtrar datos con valores físicamente imposible, según las indicaciones de la BSRN (mirar en el libro)
@@ -56,6 +72,7 @@ def main(options, args):
     schema_path = os.path.dirname(current_file)
     schema_fname = "{}/../jsons/solarnc_schema.json".format(schema_path)
     fechas = {}
+    df_stations = {}
     for s in stations:
         infiles = glob.glob("{}/{}/Minutos/*.dat".format(path, s))
         fechas[s] = {}
@@ -81,13 +98,28 @@ def main(options, args):
                     print(2, i, f, fechas[s][fecha], file=flog, sep=',')
         minima = min(fechas[s]) if 'minima' not in locals() else min(min(fechas[s]), minima)
         maxima = max(fechas[s]) if 'maxima' not in locals() else max(max(fechas[s]), maxima)
-        # ordenadas = sorted(fechas)
+
+        ordenadas = sorted(fechas[s]) # en verdad no tendría por qué hacer falta que estén ordenadas.
+        # se tiene que añadir la opción de que para un mismo día exista más de un fichero
+        for d in ordenadas:
+            df = getFromFile(fechas[s][d])
+            if s not in df_stations:
+                columnas = df.columns
+                df_stations[s] = pd.DataFrame(columns=columnas)
+            df_stations[s] = pd.concat([df_stations[s], df], ignore_index=True)
+
     fecha_i = minima + np.timedelta64(0, 'm')
     fecha_fin = np.datetime64(maxima + 1) - np.timedelta64(1, 'm')
     ffechas = os.path.dirname(os.path.realpath(__file__)) + "/../logs/" + 'fechas.txt'
     ffechas = open(ffechas, 'w')
+    df = df_stations['METAS']
+    df.set_index(columnas[0], inplace=True)
     while fecha_i <= fecha_fin:
-        print(fecha_i, file=ffechas)
+        try:
+            esta = df.loc[fecha_i]
+        except:
+            print(3, fecha_i, file=flog, sep=',')
+        #        print(fecha_i, file=ffechas)
         fecha_i += np.timedelta64(1, 'm')
         # TODO obtener los datos de esa marca temporal para cada una de las estaciones, incrementar en minutos
 
